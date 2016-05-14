02c81baa05f2e7b742cbf4057584d8ef531fa660
yevgeniy.zamkovenko <evgeniyjames@gmail.com>
2016-05-14 23:21:05 +0300

    -set new URL_MAIN

--- app/src/main/java/com/yevgeniyzamkovenko/taro/utils/IDefines.java
+++ app/src/main/java/com/yevgeniyzamkovenko/taro/utils/IDefines.java
@@ -10,7 +10,7 @@ public interface IDefines {
     String CLIENT_ID = "1";
     String CLIENT_SECRET = "1";
 
-    String URL_MAIN = "http://faydaentest.somee.com";
+    String URL_MAIN = "http://mrtest71.somee.com";
     String URL_LOGIN = URL_MAIN + "/en/home/login";
     String URL_REDIRECT = URL_MAIN + "/blunk.htm";
 
